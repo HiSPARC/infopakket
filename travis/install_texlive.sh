29f13e33829f89f5b32d1d9f8c2739244b39acdf
Arne de Laat <arne@delaat.net>
2017-12-06 08:36:42 +0100

    Try with less cd

--- travis/install_texlive.sh
+++ travis/install_texlive.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env sh
-
+#!/usr/bin/env bash
 set -e
+
 export PATH=/tmp/texlive/bin/x86_64-linux:$PATH
 
 # See if there is a cached version of TL available
@@ -8,7 +8,7 @@ if ! command -v latexmk > /dev/null 2>&1; then
   wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
   tar -xzf install-tl-unx.tar.gz
   cd install-tl-20*
-  ./install-tl --profile=../texlive.profile
+  ./install-tl --profile=../travis/texlive.profile
 fi
 
 # Keep no backups (smaller cache)
